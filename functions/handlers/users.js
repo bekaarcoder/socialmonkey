08b05b65d22ac1b9fcd33337af3446dc2646da29
Shashank <s_shashank@outlook.com>
2019-07-22 23:41:27 +0530

    get user details

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -125,6 +125,33 @@ exports.addUserDetails = (req, res) => {
     });
 };
 
+exports.getAuthenticatedUser = (req, res) => {
+  let userData = {};
+
+  db.collection("users")
+    .doc(req.user.handle)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        userData.credentials = doc.data();
+        return db
+          .collection("likes")
+          .where("userHandle", "==", req.user.handle)
+          .get();
+      }
+    })
+    .then(data => {
+      userData.likes = [];
+      data.forEach(doc => {
+        userData.likes.push(doc.data());
+      });
+      return res.json(userData);
+    })
+    .catch(error => {
+      return res.status(500).json({ error: error.code });
+    });
+};
+
 exports.uploadImage = (req, res) => {
   const busboy = new BusBoy({ headers: req.headers });
 
