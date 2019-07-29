8efd8b5da54a086a5b926b222ea55a28d8e6d817
Shashank <s_shashank@outlook.com>
2019-07-29 23:31:10 +0530

    creating notifications

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
+const { db } = require("./utils/admin");
 
 const firebaseAuth = require("./utils/fbauth");
 const {
@@ -53,3 +54,78 @@ app.post("/user", firebaseAuth, addUserDetails);
 app.get("/user", firebaseAuth, getAuthenticatedUser);
 
 exports.api = functions.https.onRequest(app);
+
+exports.createLikeNotification = functions.firestore
+  .document("likes/{id}")
+  .onCreate(snapshot => {
+    db.collection("shouts")
+      .doc(snapshot.data().shoutId)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return db
+            .collection("notifications")
+            .doc(snapshot.id)
+            .set({
+              createdAt: new Date().toISOString(),
+              recipient: doc.data().userHandle,
+              sender: snapshot.data().userHandle,
+              type: "like",
+              read: false,
+              shoutId: doc.id
+            });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch(error => {
+        console.log(error);
+        return;
+      });
+  });
+
+exports.createCommentNotification = functions.firestore
+  .document("comments/{id}")
+  .onCreate(snapshot => {
+    db.collection("shouts")
+      .doc(snapshot.data().shoutId)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          return db
+            .collection("notifications")
+            .doc(snapshot.id)
+            .set({
+              createdAt: new Date().toISOString(),
+              recipient: doc.data().userHandle,
+              sender: snapshot.data().userHandle,
+              type: "comment",
+              read: false,
+              shoutId: doc.id
+            });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch(error => {
+        console.log(error);
+        return;
+      });
+  });
+
+exports.createUnlikeNotification = functions.firestore
+  .document("likes/{id}")
+  .onDelete(snapshot => {
+    db.collection("notifications")
+      .doc(snapshot.id)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch(error => {
+        console.log(error);
+        return;
+      });
+  });
